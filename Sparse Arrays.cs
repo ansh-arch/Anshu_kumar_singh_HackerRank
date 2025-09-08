e593e3df40bc5e64d5e85bb5336f7eb543152413
Anshu Kumar Singh <120318440+ansh-arch@users.noreply.github.com>
2025-09-09 00:30:09 +0530

    Sparse Arrays Create

---
+++ Sparse Arrays.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ConsoleApp2
+{
+    internal class Sparse_Arrays
+    {
+        public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            List<int> lst = new List<int>();
+            foreach (string s in stringList)
+            {
+                if (dic.ContainsKey(s))
+                {
+                    dic[s]++;
+                }
+                else
+                {
+                    dic[s] = 1;
+                }
+            }
+            foreach (string ss in queries)
+            {
+                lst.Add(dic.ContainsKey(ss) ? dic[ss] : 0);
+            }
+            return lst;
+        }
+    }
+}
