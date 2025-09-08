cde1a820c0af30d6979a7ddfca2ddbc1a3889acf
Anshu Kumar Singh <120318440+ansh-arch@users.noreply.github.com>
2025-09-09 00:04:46 +0530

    Left Rotation of Array Create

---
+++ LeftRotationArray.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ConsoleApp2
+{
+    internal class LeftRotationArray
+    {
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            int n = arr.Count;
+            List<int> lst = new List<int>(new int[n]);
+            for (int i = 0; i < n; i++)
+            {
+                int newIndex = (i + n - d) % n;
+                lst[newIndex] = arr[i];
+            }
+            return lst;
+        }
+    }
+}
