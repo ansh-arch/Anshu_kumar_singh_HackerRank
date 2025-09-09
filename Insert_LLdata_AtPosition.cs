71319a9bdccfe77894ecce8f6ad5a04ab4ff7811
Anshu Kumar Singh <120318440+ansh-arch@users.noreply.github.com>
2025-09-09 22:20:34 +0530

    Insert at Position
    Insert a node at a specific position in a linked list

---
+++ Insert_LLdata_AtPosition.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace ConsoleApp2
+{
+    internal class Insert_LLdata_AtPosition
+    {
+        static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+        {
+            // Returns the input list without modification; actual logic to be implemented here.
+            SinglyLinkedListNode toAdd = new SinglyLinkedListNode(data);
+            if (llist == null)
+            {
+                return llist;
+            }
+            SinglyLinkedListNode prevNode = llist;
+            for (int i = 0; i < position - 1; i++)
+            {
+                prevNode = prevNode.next;
+            }
+            toAdd.next = prevNode.next;
+            prevNode.next = toAdd;
+
+            return llist;
+        }
+    }
+}
