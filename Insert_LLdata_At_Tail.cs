5cc7bae50dd5adee6b619cc48dc6917d5966273c
Anshu Kumar Singh <120318440+ansh-arch@users.noreply.github.com>
2025-09-09 22:02:14 +0530

    Insert a Node at the Tail of a Linked List

---
+++ Insert_LLdata_At_Tail.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ConsoleApp2
+{
+    internal class Insert_LLdata_At_Tail
+    {
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        {
+            SinglyLinkedListNode toAdd = new SinglyLinkedListNode(data);
+            if (head == null)
+            {
+                return toAdd;
+            }
+            SinglyLinkedListNode curr = head;
+            while (curr.next != null)
+            {
+                curr = curr.next;
+            }
+            //This while loop terminates after getting last node
+            curr.next = toAdd;
+            return head;
+
+
+        }
+    }
+}
