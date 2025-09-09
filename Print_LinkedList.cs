83e4406dc0ee839a760b82da5c0c57ab367999b8
Anshu Kumar Singh <120318440+ansh-arch@users.noreply.github.com>
2025-09-09 22:00:07 +0530

    Print the Elements of a Linked List

---
+++ Print_LinkedList.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConsoleApp2
+{
+    internal class Print_LinkedList
+    {
+        static void printLinkedList(SinglyLinkedListNode head)
+        {
+            while (head != null)
+            {
+                Console.WriteLine(head.data);
+                head = head.next;
+            }
+
+        }
+    }
+}
